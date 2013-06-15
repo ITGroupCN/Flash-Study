edc13858288e58013d2b7f36053f54b13574fffd
ashwin153 <ashwin.madavan@gmail.com>
2013-06-15 12:53:45 -0700

    Updated Set and Flashcard objects.

--- App Project/src/com/flashstudy/flashcard/Flashcard.java
+++ App Project/src/com/flashstudy/flashcard/Flashcard.java
@@ -2,4 +2,23 @@ package com.flashstudy.flashcard;
 
 public class Flashcard {
 
+	private String _term, _definition;
+	
+	public Flashcard(String term, String definition) {
+		_term = term;
+		_definition = definition;
+	}
+	
+	public String getTerm() {
+		return _term;
+	}
+	
+	public String getDefinition() {
+		return _definition;
+	}
+	
+	@Override
+	public String toString() {
+		return _term + ":" + _definition;
+	}
 }
--- App Project/src/com/flashstudy/flashcard/Set.java
+++ App Project/src/com/flashstudy/flashcard/Set.java
@@ -1,5 +1,29 @@
 package com.flashstudy.flashcard;
 
+import java.util.ArrayList;
+
 public class Set {
 
+	private ArrayList<Flashcard> _flashcards;
+	
+	public Set(ArrayList<Flashcard> flashcards) {
+		_flashcards = flashcards;
+	}
+	
+	public void addFlashcard(Flashcard flashcard) {
+		_flashcards.add(flashcard);
+	}
+	
+	public Flashcard getFlashcard(int index) {
+		return _flashcards.get(index);
+	}
+	
+	public ArrayList<Flashcard> getAllFlashcards() {
+		return _flashcards;
+	}
+	
+	@Override
+	public String toString() {
+		return _flashcards.toString();
+	}
 }
