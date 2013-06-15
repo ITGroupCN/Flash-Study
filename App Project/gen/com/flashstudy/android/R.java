4086625412d32a05899423252283b675ae30e11c
ashwin153 <ashwin.madavan@gmail.com>
2013-06-15 15:14:57 -0700

    Updated List

--- App Project/gen/com/flashstudy/android/R.java
+++ App Project/gen/com/flashstudy/android/R.java
@@ -24,10 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int ListElement_dateCreated=0x7f080006;
+        public static final int ListElement_description=0x7f080007;
+        public static final int ListElement_name=0x7f080005;
+        public static final int ListElement_numFlashcard=0x7f080009;
+        public static final int ListElement_type=0x7f080008;
+        public static final int MenuActivity_buttonPanel=0x7f080002;
+        public static final int MenuActivity_cameraUploadButton=0x7f080003;
+        public static final int MenuActivity_formUploadButton=0x7f080004;
+        public static final int MenuActivity_logo=0x7f080000;
+        public static final int MenuActivity_setList=0x7f080001;
+        public static final int action_settings=0x7f08000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_menu=0x7f030000;
+        public static final int list_element_flashcard=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,6 +46,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int camera_upload=0x7f050003;
+        public static final int form_upload=0x7f050004;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
