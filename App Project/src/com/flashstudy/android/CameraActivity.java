d839ff52c94d6b68719b5b7bd09430b22863f194
Karthik Bala <drmelonhead@gmail.com>
2013-06-15 20:59:18 -0700

    working on camera activity

--- App Project/src/com/flashstudy/android/CameraActivity.java
+++ App Project/src/com/flashstudy/android/CameraActivity.java
@@ -1,17 +1,23 @@
 package com.flashstudy.android;
 
 import android.os.Bundle;
+import android.app.Activity;
 
+
+import com.evernote.client.android.EvernoteSession;
 import com.evernote.client.android.EvernoteSession.EvernoteService;
 
-public class CameraActivity {
+public class CameraActivity extends Activity {
 
 	// Connect to evernote
 	private static final String CONSUMER_KEY = "shravvmehtaa";
 	private static final String CONSUMER_SECRET = "fde31f0694d0134d";
-	private static final EvernoteService EVERNOTE_HOST = EvernoteService.SANDBOX; // Change SANDBOX to PRODUCTION later
+	//private static final EvernoteService EVERNOTE_HOST = EvernoteService.SANDBOX; // Change SANDBOX to PRODUCTION later
+	private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
 	
 	public void onCreate(Bundle savedInstanceState) {
+		
+		EvernoteSession mEvernoteSession = EvernoteSession.getInstance(getApplicationContext(), CONSUMER_KEY, CONSUMER_SECRET, EVERNOTE_SERVICE);
 		
 		
 	}
